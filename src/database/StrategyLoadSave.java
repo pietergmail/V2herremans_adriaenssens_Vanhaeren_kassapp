990edee9ff878ddff08078ff1641059491cd565d
zeno-adriaenssens <43340633+zeno-adriaenssens@users.noreply.github.com>
2020-04-24 15:33:28 +0200

    Moved database inside model package

--- src/database/StrategyLoadSave.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
-public interface StrategyLoadSave {
-    ArrayList<Object> load() throws IOException, DatabaseException;
-    void save(ArrayList<Object> objecten) throws IOException;
-}
